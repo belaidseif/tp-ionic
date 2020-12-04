4a9716c6c1f6b4d8cf0a3a8c30cadc5c9071d15d
belaid seif <belaidseif@gmail.com>
2020-12-04 20:54:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,26 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import {AngularFireModule} from '@Angular/fire';
+import {AngularFireDatabaseModule } from '@Angular/fire/database';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+export var firebaseConfig = {
+  apiKey: "AIzaSyAgALJyYlp2Ccr7JzPzb2VUrdQo9M8xfJU",
+  authDomain: "todo-gl-3a646.firebaseapp.com",
+  projectId: "todo-gl-3a646",
+  storageBucket: "todo-gl-3a646.appspot.com",
+  messagingSenderId: "485228983365",
+  appId: "1:485228983365:web:482087862e573078dd5f2c"
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/database';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
+export class HomePage implements OnInit {
+  currentDate :string;
+  newTask: string;
+  allTasks = []
+  constructor(private angFire: AngularFireDatabase) {
+    const todayDate = new Date()
+    const options = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    }
+    this.currentDate = todayDate.toLocaleDateString('en-en', options)
+  }
+  ngOnInit(){
+    this.getTask()
+  }
+  getTask(){
+    this.angFire.list('Tasks/').snapshotChanges(['child_added']).subscribe(reponse => {
+      this.allTasks = []
+      reponse.forEach(element => {
+        console.log(element.payload);
+        this.allTasks.push({
+          key: element.key,
+          text: element.payload.exportVal().text,
+          checked: element.payload.exportVal().checked,
+          date: element.payload.exportVal().date.substring(11, 16)
+        })
+      })
+      
+    })
+  }
+  addNewTask(){
+    this.angFire.list('Tasks/').push({
+      text:this.newTask,
+      checked: false,
+      date: new Date().toISOString()
+    })
+    this.newTask = ''
+  }
+  changeCheckedState(task){
+    this.angFire.object(`Tasks/${task.key}/checked`).set(task.checked)
+  }
 
 }
