7d869c0168ddcae42ebe8ac4bc946c4bf883007e
belaid seif <belaidseif@gmail.com>
2020-12-05 20:48:45 +0100

    finish

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -18,7 +18,14 @@ export class RegisterPage implements OnInit {
   async onRegister(form: NgForm){
     console.log(form.valid);
     if(form.valid){
-      this.authService.register(form.value.email, form.value.password)
+      this.authService.register(form.value.email, form.value.password).catch(async err => {
+        const alert = await this.alertCtrl.create({
+          header: 'error',
+          message: err.message,
+          buttons: ['OK']
+        })
+        await alert.present()
+      })
     }else{
       const alert = await this.alertCtrl.create({
         header: 'email not valid',
